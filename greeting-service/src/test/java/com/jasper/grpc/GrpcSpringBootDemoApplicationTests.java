3982af0691fe91fb345af6bec399e86c963cfd67
JINBO <kimpa0107@163.com>
2022-04-09 10:49:59 +0800

    init

---
+++ greeting-service/src/test/java/com/jasper/grpc/GrpcSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasper.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
